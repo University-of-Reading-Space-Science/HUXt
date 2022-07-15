efa179a4afc1aef37f2371b7665d093f40d18903
mathewjowens <mathewjowens@gmail.com>
2022-07-15 14:18:37 +0100

    streakline test
    Added reference model output for a set of streaklines and code to make comparison with current installation

--- code/test_huxt.py
+++ code/test_huxt.py
@@ -118,4 +118,51 @@ def test_time_dependent():
     assert np.allclose(arrival_stats_test['v'], v_ref)
 
     return
+
+
+def test_streaklines():
+    """
+    Test HUXt against a reference HUXt solution for a structured solar wind inner boundary with a ConeCME addded.
+    Checks consistency of the HUXt flow field, and the streakline positions
+    """
     
+    v_boundary = np.ones(128) * 400 * (u.km/u.s)
+    v_boundary[30:50] = 600 * (u.km/u.s)
+    v_boundary[95:125] = 700 * (u.km/u.s)
+
+    #  Add a CME
+    cme = H.ConeCME(t_launch=0.5*u.day, longitude=0.0*u.deg, width=30*u.deg, v=1000*(u.km/u.s), thickness=5*u.solRad)
+    cme_list = [cme]
+
+    #  Setup HUXt to do a 5-day simulation, with model output every 4 timesteps (roughly half and hour time step)
+    model_test = H.HUXt(v_boundary=v_boundary, cr_num=2080, cr_lon_init=180*u.deg, simtime=5*u.day, dt_scale=4)
+
+
+    #trace a bunch of field lines from a range of evenly spaced Carrington longitudes
+    dlon = (20*u.deg).to(u.rad).value
+    lon_grid = np.arange(dlon/2, 2*np.pi-dlon/2 + 0.0001, dlon)*u.rad
+
+
+    #give the streakline footpoints (in Carr long) to the solve method
+    model_test.solve(cme_list, streak_carr = lon_grid)
+        
+        
+    #load in the test data
+    
+    dirs = H._setup_dirs_()
+    test_case_path = os.path.join(dirs['test_data'], 'HUXt_CR2080_streaklines_case.hdf5')
+    h5f = h5py.File(test_case_path,'r')
+    vgrid = np.array(h5f['vgrid'])
+    streakline_particles_r = np.array(h5f['streak_particles_r'])
+    h5f.close()
+    
+    
+    #check the data agree - first check the vgrid is the same to machine tol
+    assert np.allclose(model_test.v_grid.value, vgrid)
+    
+    #only compare non-nan values of streakline positions
+    mask = np.isfinite(streakline_particles_r)
+    assert np.allclose(streakline_particles_r[mask], 
+                       model_test.streak_particles_r[mask].value)
+    
+    return
