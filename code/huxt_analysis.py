0df1766af16f746b96a069001a6bc63a49edfa79
Luke Barnard <yq904481@reading.ac.uk>
2022-07-28 10:45:09 +0100

    Updated 2d and 3d plot routines for better text positioning and consistent plotting of bodies. Also updated requirements for dependabot alerts

--- code/huxt_analysis.py
+++ code/huxt_analysis.py
@@ -107,28 +107,35 @@ def plot(model, time, save=False, tag='', fighandle=np.nan, axhandle=np.nan,
     ax.set_yticklabels([])
     ax.set_xticklabels([])
     
+    
     if not minimalplot:
         # determine which bodies should be plotted
-        plot_observers = zip(['EARTH', 'VENUS', 'MERCURY', 'STA', 'STB'],
-                             ['ko', 'mo', 'co', 'rs', 'y^'])
-        if model.r[0] > 200 *u.solRad:
-            plot_observers = zip(['EARTH', 'MARS', 'JUPITER', 'SATURN'],
-                                 ['ko', 'mo', 'ro', 'cs'])
-    
+        bodies = get_body_styles()
         
         # Add on observers 
-        for body, style in plot_observers:
+        for body, style in bodies.items():
             obs = model.get_observer(body)
             deltalon = 0.0*u.rad
             if model.frame == 'sidereal':
                 earth_pos = model.get_observer('EARTH')
                 deltalon = earth_pos.lon_hae[id_t] - earth_pos.lon_hae[0]
-                
+            
             obslon = H._zerototwopi_(obs.lon[id_t] + deltalon)
-            ax.plot(obslon, obs.r[id_t], style, markersize=16, label=body)
-    
+                
+            if body == 'STA':
+                maker = 's'
+            elif body == 'STB':
+                marker = '^'
+            else:
+                marker = 'o'
+                
+            if body == 'EARTH':
+                ax.plot(obslon, obs.r[id_t], style['marker'], color=style['color'], markersize=16, label=body)
+            elif (obs.r[id_t] > model.r.min()) & (obs.r[id_t] < model.r.max()):
+                ax.plot(obslon, obs.r[id_t],  style['marker'], color=style['color'], markersize=16, label=body)
+                
         # Add on a legend.
-        fig.legend(ncol=5, loc='lower center', frameon=False, handletextpad=0.2, columnspacing=1.0)
+        ax.legend(bbox_to_anchor=(0.5, -0.15), loc="center", ncol=5, frameon=False, handletextpad=0.0, columnspacing=0.0)
         
         ax.patch.set_facecolor('slategrey')
         fig.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.99)
@@ -148,11 +155,11 @@ def plot(model, time, save=False, tag='', fighandle=np.nan, axhandle=np.nan,
         # Add label
         label = "   Time: {:3.2f} days".format(model.time_out[id_t].to(u.day).value)
         label = label + '\n ' + (model.time_init + time).strftime('%Y-%m-%d %H:%M')
-        fig.text(0.70, pos.y0, label, fontsize=16)
+        ax.text(0.725, 0.0, label, fontsize=16, transform=ax.transAxes)
         
         label = "HUXt2D \nLat: {:3.0f} deg".format(model.latitude.to(u.deg).value)
-        fig.text(0.175, pos.y0, label, fontsize=16)
-
+        ax.text(0.1, 0.0, label, fontsize=16, transform=ax.transAxes)
+        
         # plot any tracked streaklines
         if model.track_streak:
             nstreak = len(model.streak_particles_r[0,:,0,0])
@@ -590,9 +597,7 @@ def plot3d_radial_lat_slice(model3d, time, lon=np.NaN*u.deg, save=False, tag='')
     # Set edge color of contours the same, for good rendering in PDFs
     for c in cnt.collections:
         c.set_edgecolor("face")
-             
-     
-        
+                
     # Trace the CME boundaries
     cme_colors = ['r', 'c', 'm', 'y', 'deeppink', 'darkorange']
     for n in range(0, len(model.cmes)):
@@ -608,8 +613,6 @@ def plot3d_radial_lat_slice(model3d, time, lon=np.NaN*u.deg, save=False, tag='')
             cme_r_front[ilat] = model.cme_particles_r[n,id_t,0,id_lon]
             cme_r_back[ilat] = model.cme_particles_r[n,id_t,1,id_lon]
         
-            #ax.plot(model.latitude.to(u.rad), (r_front*u.km).to(u.solRad), 'o', color=cme_colors[n], linewidth=3)
-            #ax.plot(model.latitude.to(u.rad), (r_back*u.km).to(u.solRad), 'o', color=cme_colors[n], linewidth=3)
         #trim the nans
         # Find indices that sort the longitudes, to make a wraparound of lons
         id_sort_inc = np.argsort(lats)
@@ -640,28 +643,26 @@ def plot3d_radial_lat_slice(model3d, time, lon=np.NaN*u.deg, save=False, tag='')
 
 
     # determine which bodies should be plotted
-    plot_observers = zip(['EARTH', 'VENUS', 'MERCURY', 'STA', 'STB'],
-                         ['ko', 'mo', 'co', 'rs', 'y^'])
-    if model.r[0] > 200 *u.solRad:
-        plot_observers = zip(['EARTH', 'MARS', 'JUPITER', 'SATURN'],
-                             ['ko', 'mo', 'ro', 'cs'])
-    
+    bodies = get_body_styles()
+
     # Add on observers 
-    for body, style in plot_observers:
+    for body, style in bodies.items():
         obs = model.get_observer(body)
-        deltalon = 0.0*u.rad
         
-        #adjust body longitude for the frame
-        if model.frame == 'sidereal':
-            earth_pos = model.get_observer('EARTH')
-            deltalon = earth_pos.lon_hae[id_t] - earth_pos.lon_hae[0]  
-        bodylon = H._zerototwopi_(obs.lon[id_t] + deltalon)*u.rad
-        #plot bodies that are close to being in the plane
-        if abs(bodylon - lon_out) < model.dlon *2:
-            ax.plot(obs.lat[id_t], obs.r[id_t], style, markersize=16, label=body)
-    
+        if body == 'STA':
+            maker = 's'
+        elif body == 'STB':
+            marker = '^'
+        else:
+            marker = 'o'
+
+        if body == 'EARTH':
+            ax.plot(obs.lat[id_t], obs.r[id_t], style['marker'], color=style['color'], markersize=16, label=body)
+        elif (obs.r[id_t] > model.r.min()) & (obs.r[id_t] < model.r.max()):
+            ax.plot(obs.lat[id_t], obs.r[id_t],  style['marker'], color=style['color'], markersize=16, label=body)
+
     # Add on a legend.
-    fig.legend(ncol=5, loc='lower center', frameon=False, handletextpad=0.2, columnspacing=1.0)
+    ax.legend(bbox_to_anchor=(0.5, -0.15), loc="center", ncol=5, frameon=False, handletextpad=0.0, columnspacing=0.0)
     
     ax.patch.set_facecolor('slategrey')
     fig.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.99)
@@ -688,11 +689,11 @@ def plot3d_radial_lat_slice(model3d, time, lon=np.NaN*u.deg, save=False, tag='')
     # Add label
     label = "   Time: {:3.2f} days".format(model.time_out[id_t].to(u.day).value)
     label = label + '\n ' + (model.time_init + time).strftime('%Y-%m-%d %H:%M')
-    fig.text(0.70, pos.y0, label, fontsize=16)
-    
-    label = "HUXt3D \nLong: {:3.1f} deg".format(lon_out.to(u.deg).value)
-    fig.text(0.175, pos.y0, label, fontsize=16)
+    ax.text(0.725, 0.0, label, fontsize=16, transform=ax.transAxes)
 
+    label = "HUXt3D \nLong: {:3.1f} deg".format(lon_out.to(u.deg).value)
+    ax.text(0.1, 0.0, label, fontsize=16, transform=ax.transAxes)
+    
     if save:
         cr_num = np.int32(model.cr_num.value)
         filename = "HUXt_CR{:03d}_{}_frame_{:03d}.png".format(cr_num, tag, id_t)
@@ -828,24 +829,32 @@ def plot_bpol(model, time, save=False, tag='', fighandle=np.nan, axhandle=np.nan
     if not minimalplot:
         
         # determine which bodies should be plotted
-        plot_observers = zip(['EARTH', 'VENUS', 'MERCURY', 'STA', 'STB'],
-                             ['ko', 'mo', 'co', 'rs', 'y^'])
-        if model.r[0] > 200 *u.solRad:
-            plot_observers = zip(['EARTH', 'MARS', 'JUPITER', 'SATURN'],
-                                 ['ko', 'mo', 'ro', 'cs'])
+        bodies = get_body_styles()
+        
         # Add on observers 
-        for body, style in plot_observers:
+        for body, style in bodies.items():
             obs = model.get_observer(body)
             deltalon = 0.0*u.rad
             if model.frame == 'sidereal':
                 earth_pos = model.get_observer('EARTH')
                 deltalon = earth_pos.lon_hae[id_t] - earth_pos.lon_hae[0]
-                
+            
             obslon = H._zerototwopi_(obs.lon[id_t] + deltalon)
-            ax.plot(obslon, obs.r[id_t], style, markersize=16, label=body)
-    
+                
+            if body == 'STA':
+                maker = 's'
+            elif body == 'STB':
+                marker = '^'
+            else:
+                marker = 'o'
+                
+            if body == 'EARTH':
+                ax.plot(obslon, obs.r[id_t], style['marker'], color=style['color'], markersize=16, label=body)
+            elif (obs.r[id_t] > model.r.min()) & (obs.r[id_t] < model.r.max()):
+                ax.plot(obslon, obs.r[id_t],  style['marker'], color=style['color'], markersize=16, label=body)
+                
         # Add on a legend.
-        fig.legend(ncol=5, loc='lower center', frameon=False, handletextpad=0.2, columnspacing=1.0)
+        ax.legend(bbox_to_anchor=(0.5, -0.15), loc="center", ncol=5, frameon=False, handletextpad=0.0, columnspacing=0.0)
         
         ax.patch.set_facecolor('slategrey')
         fig.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.99)
@@ -865,10 +874,10 @@ def plot_bpol(model, time, save=False, tag='', fighandle=np.nan, axhandle=np.nan
         # Add label
         label = "   Time: {:3.2f} days".format(model.time_out[id_t].to(u.day).value)
         label = label + '\n ' + (model.time_init + time).strftime('%Y-%m-%d %H:%M')
-        fig.text(0.70, pos.y0, label, fontsize=16)
+        ax.text(0.725, 0.0, label, fontsize=16, transform=ax.transAxes)
         
-        label = "HUXt2D"
-        fig.text(0.175, pos.y0, label, fontsize=16)
+        label = "HUXt2D \nLat: {:3.0f} deg".format(model.latitude.to(u.deg).value)
+        ax.text(0.1, 0.0, label, fontsize=16, transform=ax.transAxes)
         
         # plot any tracked streaklines
         if model.track_streak:
@@ -908,3 +917,19 @@ def plot_bpol(model, time, save=False, tag='', fighandle=np.nan, axhandle=np.nan
         fig.savefig(filepath)
 
     return fig, ax
+
+def get_body_styles():
+    """
+    Return a dictionary of observer/body names with associated colors and markerstyles
+    """
+    
+    body_styles = {'MERCURY':{'color':'black','marker':'o'},
+                   'VENUS':{'color':'tan','marker':'o'},
+                   'EARTH':{'color':'c','marker':'o'},
+                   'MARS':{'color':'darkred','marker':'o'},
+                   'JUPITER':{'color':'orange','marker':'o'},
+                   'SATURN':{'color':'m','marker':'o'},
+                   'STA':{'color':'r','marker':'s'},
+                   'STB':{'color':'y','marker':'^'}}
+    
+    return body_styles
