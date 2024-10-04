f57ae37bc2a153aadffeb40ccce24d4a9615a8e6
mathewjowens <31659031+mathewjowens@users.noreply.github.com>
2024-10-04 10:36:37 +0100

    Update huxt.py

--- code/huxt.py
+++ code/huxt.py
@@ -1873,11 +1873,12 @@ def _is_in_cme_boundary_expanding_(r_boundary, lon, lat, time, cme_params):
                               np.cos(lat) * np.cos(cme_lat) * np.cos(delta_long))
 
     if central_angle <= ang_width:
-        # compute the fractional distance from nose to tail
+        isincme = True
+        #also compute the fractional distance from nose to tail
         r_of_nose = cme_v * (time - cme_t_launch)
         nose_to_tail_r = (2 * cme_radius + cme_thickness)
         dist_from_nose = r_of_nose / nose_to_tail_r
-        isincme = True
+
 
     return isincme, dist_from_nose
 
